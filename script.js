c05a6e9cf13145c6c80c4b704ff423bb42bbf7d3
Thaua <thaua19felipe@gmail.com>
2023-05-18 11:29:44 -0300

    um pouco de vida ao projeto

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
-let button = document.getElementById('switch')
+let buttonContainer = document.getElementById('switch-container')
+let buttonSwitch = document.getElementById('switch-button')
+let button = document.querySelector('#switch-button img')
 let body = document.getElementById('body')
 let logo = document.getElementById('logo')
 
-button.addEventListener('click', () => {
+buttonContainer.addEventListener('click', () => {
 
-    body.classList.toggle('body-light-theme')
+    body.classList.toggle('light')
+    
+    if (body.classList.contains('light')) {
 
-    if (logo.classList.contains('logo-dark-theme')) {
-
+        buttonSwitch.classList.remove('switch-dark')
+        buttonSwitch.classList.add('switch-light')
         logo.src="images/logo/logo-preta.png"
         button.src="images/switch-light.png"
-
-        body.style.setProperty('--card-background-color', 'rgba(0, 0, 0, 0.1)')
-        body.style.setProperty('--card-border-color', 'rgba(0, 0, 0, 0.5)')
-        body.style.setProperty('--primary-color', '#131313')
-        body.style.setProperty('--secondary-color', '#FFFF')
-
-        logo.classList.toggle('logo-dark-theme')
-
+    
     } else {
 
+        buttonSwitch.classList.remove('switch-light')
+        buttonSwitch.classList.add('switch-dark')
         logo.src="images/logo/logo-branca.png"
         button.src="images/switch-dark.png"
-
-        body.style.setProperty('--card-background-color', 'rgba(255, 255, 255, 0.1)')
-        body.style.setProperty('--card-border-color', 'rgba(255, 255, 255, 0.5)')
-        body.style.setProperty('--primary-color', '#FFFF')
-        body.style.setProperty('--secondary-color', '#000')
-
-        logo.classList.toggle('logo-dark-theme')
-
+        
     }
 
 })
